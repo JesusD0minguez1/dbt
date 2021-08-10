6985f21a6406aea07bdef0979944607a6a8a714c
dabbmg <dabb.mg@gmail.com>
2021-08-10 15:19:37 -0600

    Update RiddleScreen.java
    Added ArrayLists for questions and riddles

--- app/src/main/java/com/example/dbt/RiddleScreen.java
+++ app/src/main/java/com/example/dbt/RiddleScreen.java
@@ -1,10 +1,17 @@
 package com.example.dbt;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+
 
 public class RiddleScreen extends AppCompatActivity {
+
+
+    private ArrayList<String> riddles = new ArrayList<String>();
+    private ArrayList<String> questions = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
