0b120723d9d60f72ba4ad3412e6debf071bcd6c7
dabbmg <dabb.mg@gmail.com>
2021-08-11 11:20:36 -0600

    Started work on riddle
    Added trackProgress()

--- app/src/main/java/com/example/dbt/RiddleScreen.java
+++ app/src/main/java/com/example/dbt/RiddleScreen.java
@@ -3,6 +3,8 @@ package com.example.dbt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
 import java.util.ArrayList;
 
 
@@ -11,11 +13,17 @@ public class RiddleScreen extends AppCompatActivity {
 
     private ArrayList<String> riddles = new ArrayList<String>();
     private ArrayList<String> questions = new ArrayList<String>();
+    private ProgressBar progBar = (ProgressBar) findViewById(R.id.progressBar);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.riddle_screen);
+    }
+
+
+    private void trackProgress() {
+        progBar.setMax(riddles.size());
     }
 }
