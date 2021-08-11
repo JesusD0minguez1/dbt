373305ee995115aca13037b05dd36668cedf454a
dabbmg <dabb.mg@gmail.com>
2021-08-11 13:46:00 -0600

    Fake information to populate riddles
    Populating fake information in riddles and questions so that we can start testing code before we even have the real riddles

--- app/src/main/java/com/example/dbt/RiddleScreen.java
+++ app/src/main/java/com/example/dbt/RiddleScreen.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class RiddleScreen extends AppCompatActivity {
 
 
     private ArrayList<String> riddles = new ArrayList<String>();
-    private ArrayList<String> questions = new ArrayList<String>();
+    private HashMap<Integer, String> questions = new HashMap<Integer, String>();
     private ProgressBar progBar = (ProgressBar) findViewById(R.id.progressBar);
 
 
@@ -20,6 +21,26 @@ public class RiddleScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.riddle_screen);
+    }
+
+
+    private void populateRiddles() {
+        riddles.add("Test Riddle 1");
+        riddles.add("Test Riddle 2");
+        riddles.add("test Riddle 3");
+    }
+
+
+    private void populateQuestions() {
+        questions.put(0,"Riddle 1 : Question 1");
+        questions.put(0, "Riddle 2 : Question 1");
+        questions.put(0, "Riddle 3 : Question 1");
+        questions.put(1,"Riddle 1 : Question 2");
+        questions.put(1, "Riddle 2 : Question 2");
+        questions.put(1, "Riddle 3 : Question 2");
+        questions.put(2,"Riddle 1 : Question 3");
+        questions.put(2, "Riddle 2 : Question 3");
+        questions.put(2, "Riddle 3 : Question 3");
     }
 
 
