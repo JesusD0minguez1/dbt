0fff6ac82af44311b4bbd0be8f927b7951bc1eb0
jedominguez <78039980+YELO-icy@users.noreply.github.com>
2021-08-11 17:02:13 -0600

    Revert "Revert "here some stuff""
    This reverts commit 6972c81540987c35f72f7c2e7c928f39aa2889b0.

--- app/src/main/java/com/example/dbt/RiddleScreen.java
+++ app/src/main/java/com/example/dbt/RiddleScreen.java
@@ -2,7 +2,9 @@ package com.example.dbt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,8 +14,8 @@ import java.util.HashMap;
 public class RiddleScreen extends FileManager {
 
 
-    private HashMap<Integer, String> riddles = new HashMap<Integer, String>(3);
-    private HashMap<Integer, String> questions = new HashMap<Integer, String>(9);
+    private HashMap<Integer, String> riddleAnswer = new HashMap<Integer, String>(9);
+    private HashMap<Integer, String> riddles = new HashMap<Integer, String>(9);
     private ProgressBar progBar;
 
 
@@ -26,14 +28,16 @@ public class RiddleScreen extends FileManager {
         populateQuestions();
         progBar = findViewById(R.id.progressBar);
         progBar.setMax(0);
+        displayRiddles(2);
+        displayRiddlesOption();
     }
 
 
     private void populateRiddles() {
         try {
-            riddles.put(0, "Test Riddle 1");
-            riddles.put(1, "Test Riddle 2");
-            riddles.put(2, "test Riddle 3");
+            riddleAnswer.put(0, "Test Riddle 1");
+            riddleAnswer.put(1, "Test Riddle 2");
+            riddleAnswer.put(2, "test Riddle 3");
         }
         catch (Exception pr) {
             pr.printStackTrace();
@@ -43,15 +47,15 @@ public class RiddleScreen extends FileManager {
 
     private void populateQuestions() {
         try {
-            questions.put(1,"Riddle 1 : Question 1");
-            questions.put(2, "Riddle 2 : Question 1");
-            questions.put(3, "Riddle 3 : Question 1");
-            questions.put(4,"Riddle 1 : Question 2");
-            questions.put(5, "Riddle 2 : Question 2");
-            questions.put(6, "Riddle 3 : Question 2");
-            questions.put(7,"Riddle 1 : Question 3");
-            questions.put(8, "Riddle 2 : Question 3");
-            questions.put(9, "Riddle 3 : Question 3");
+            riddles.put(1,"Riddle 1 : Question 1");
+            riddles.put(2, "Riddle 2 : Question 1");
+            riddles.put(3, "Riddle 3 : Question 1");
+            riddles.put(4,"Riddle 1 : Question 2");
+            riddles.put(5, "Riddle 2 : Question 2");
+            riddles.put(6, "Riddle 3 : Question 2");
+            riddles.put(7,"Riddle 1 : Question 3");
+            riddles.put(8, "Riddle 2 : Question 3");
+            riddles.put(9, "Riddle 3 : Question 3");
         }
         catch (Exception pq) {
             pq.printStackTrace();
@@ -80,14 +84,65 @@ public class RiddleScreen extends FileManager {
             uus.printStackTrace();
         }
     }
-   /* private void displayRiddles()
+
+    private void displayRiddles(int pp)
     {
         TextView riddleDisplay = findViewById(R.id.riddleInfotxt);
-        for(int i; i < questions.size(); i++)
+        int p = 0;
+        while(p != pp + 1)
         {
-            riddleDisplay.setText(questions.keySet(i)
+            riddleDisplay.setText(riddles.get(p));
+            trackProgress(p);
         }
-    }*/
+    }
+
+    public void displayRiddlesOption()
+    {
+        try
+        {
+            RadioButton op1 = findViewById(R.id.op1);
+            RadioButton op2 = findViewById(R.id.op2);
+            RadioButton op3 = findViewById(R.id.op3);
+            RadioButton op4 = findViewById(R.id.op4);
+            RadioButton op5 = findViewById(R.id.op5);
+
+            int p = 0;
+            switch (p)
+            {
+                case 0:
+                    op1.setText(riddleAnswer.get(0));
+                    op2.setText("Option");
+                    op3.setText("dd");
+                    op4.setText("ferwer");
+                    op5.setText("rwbr");
+                    break;
+
+                case 1:
+                    op1.setText("sfsf");
+                    op2.setText(riddleAnswer.get(1));
+                    op3.setText("dd");
+                    op4.setText("ferwer");
+                    op5.setText("rwbr");
+                    break;
+                case 2:
+                    op1.setText("sfsf");
+                    op2.setText(riddleAnswer.get(2));
+                    op3.setText("dd");
+                    op4.setText("ferwer");
+                    op5.setText("rwbr");
+                    break;
+                case 3:
+                    op1.setText("sfsfes");
+                    op2.setText(riddleAnswer.get(3));
+                    op3.setText("dsfdf");
+                    op4.setText("ferwedsr");
+                    op5.setText("rwber");
+                    break;
+            }
+
+        }catch (Exception e){e.printStackTrace();}
+
+    }
 
     private void trackProgress(int newProgress) {
         try {
