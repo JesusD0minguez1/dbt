ffa3b386c5ed2ed076f4b737cea015b57bbf7ce9
dabbmg <dabb.mg@gmail.com>
2021-08-12 14:40:37 -0600

    updates
    updates

--- app/src/main/java/com/example/dbt/MemoryGame.java
+++ app/src/main/java/com/example/dbt/MemoryGame.java
@@ -3,12 +3,21 @@ package com.example.dbt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MemoryGame extends FileManager {
+
+    TextView timer;
+    TextView score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memory_game);
+
+        //Declare views
+        timer = findViewById(R.id.timerView);
+        score = findViewById(R.id.scoreView);
     }
 }
